49e16226f28ffdc74b6a069baa3f04684a231476
Brian Mason <brian.kw.mason@gmail.com>
2022-12-18 13:20:22 -0500

    Added notifications

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
 
 import {
@@ -9,9 +9,20 @@ import {
 import { useFonts as useHind, Hind_400Regular } from "@expo-google-fonts/hind";
 import { ThemeProvider } from "styled-components/native";
 import Toast from "react-native-toast-message";
+import * as Notifications from "expo-notifications";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
 import Navigation from "./src/infrastructure/navigation";
 import theme from "./src/infrastructure/theme";
 import { AuthenticationContextProvider } from "./src/infrastructure/Authentication/AuthenticationContext";
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false,
+  }),
+});
 
 export default function App() {
   const [safeAreaBackgroundColor, setSafeAreaBackgroundColor] = useState("white");
@@ -23,6 +34,45 @@ export default function App() {
   const [hindLoaded] = useHind({
     Hind_400Regular,
   });
+  async function requestPermissionsAsync() {
+    const reqNotifications = await Notifications.requestPermissionsAsync({
+      ios: { allowAlert: true, allowBadge: true, allowSound: true, allowAnnouncements: true },
+    });
+
+    return reqNotifications.granted;
+  }
+
+  const allowsNotificationsAsync = async () => {
+    const settings = await Notifications.getPermissionsAsync();
+    if (!settings.granted) {
+      requestPermissionsAsync();
+    }
+    return (
+      settings.granted || settings.ios?.status === Notifications.IosAuthorizationStatus.PROVISIONAL
+    );
+  };
+
+  const scheduleNotification = async () => {
+    const hasPushNotificationPermissionGranted = await allowsNotificationsAsync();
+    if (hasPushNotificationPermissionGranted) {
+      Notifications.cancelAllScheduledNotificationsAsync();
+      await Notifications.scheduleNotificationAsync({
+        content: {
+          title: "Time to make the planet cleaner!🌎",
+          body: "Let's make a difference🤗",
+        },
+        trigger: {
+          repeats: true,
+          hour: 13,
+          minute: 20,
+        },
+      });
+    }
+  };
+
+  useEffect(() => {
+    scheduleNotification();
+  }, []);
 
   if (!nunitoLoaded || !hindLoaded) {
     return null;
